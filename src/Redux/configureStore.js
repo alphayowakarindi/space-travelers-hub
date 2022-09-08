bde9d9aae21f6a6d4a6b4e4e5818f9afcc62f380
Alphy Gacheru <alphygacheru@gmail.com>
2022-09-08 15:53:18 +0300

    Add missionsReducer to the store

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,6 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import RocketReducers from './Rockets/Rocket';
+import missionsReducer from './missions/missions';
 
-const rootReducer = combineReducers({ RocketReducers });
+const rootReducer = combineReducers({
+  RocketReducers,
+  missions: missionsReducer,
+});
 const store = configureStore({ reducer: rootReducer });
 export default store;
